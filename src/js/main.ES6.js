5a6f55e17eac715c0ff2249b1bec2ce5f2325109
Pavel Ledovskikh <o_gredi@mail.ru>
2017-04-09 18:47:43 +0300

    почти все закончил

--- src/js/main.ES6.js
+++ src/js/main.ES6.js
@@ -1,6 +1,29 @@
 /**
  * Created by Павел on 07.04.2017.
  */
-$(document).ready(()=>{
-
+function checkNumber(number) {
+    if (number < 0) return 3;
+    if (number > 3) return 0;
+    return number
+}
+$(document).ready(() => {
+    let featureCurrent, $featureItems = $(".features__item");
+    $(".features__controls-left").click(() => {
+        featureCurrent = $(".features__item_active").index();
+        $featureItems.eq(featureCurrent).removeClass("features__item_active").addClass("features__item_next");
+        $featureItems.eq(checkNumber(featureCurrent - 1)).removeClass("features__item_prev").addClass("features__item_active");
+        $featureItems.eq(checkNumber(featureCurrent + 1)).removeClass("features__item_next");
+        $featureItems.eq(checkNumber(featureCurrent - 1) - 1).addClass("features__item_prev");
+    });
+    $(".features__controls-right").click(() => {
+        featureCurrent = $(".features__item_active").index();
+        $featureItems.eq(featureCurrent).removeClass("features__item_active").addClass("features__item_prev");
+        $featureItems.eq(checkNumber(featureCurrent + 1)).removeClass("features__item_next").addClass("features__item_active");
+        $featureItems.eq(checkNumber(featureCurrent - 1)).removeClass("features__item_prev");
+        $featureItems.eq(checkNumber(checkNumber(featureCurrent + 1) + 1)).addClass("features__item_next");
+        console.log(checkNumber(checkNumber(featureCurrent + 1)) + 1,);
+    });
+    $(".navbar-toggle").click(()=>{
+        $("#header__nav").toggle(500);
+    });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,30 @@
 /**
  * Created by Павел on 07.04.2017.
  */
-$(document).ready(function () {});
+function checkNumber(number) {
+    if (number < 0) return 3;
+    if (number > 3) return 0;
+    return number;
+}
+$(document).ready(function () {
+    var featureCurrent = void 0,
+        $featureItems = $(".features__item");
+    $(".features__controls-left").click(function () {
+        featureCurrent = $(".features__item_active").index();
+        $featureItems.eq(featureCurrent).removeClass("features__item_active").addClass("features__item_next");
+        $featureItems.eq(checkNumber(featureCurrent - 1)).removeClass("features__item_prev").addClass("features__item_active");
+        $featureItems.eq(checkNumber(featureCurrent + 1)).removeClass("features__item_next");
+        $featureItems.eq(checkNumber(featureCurrent - 1) - 1).addClass("features__item_prev");
+    });
+    $(".features__controls-right").click(function () {
+        featureCurrent = $(".features__item_active").index();
+        $featureItems.eq(featureCurrent).removeClass("features__item_active").addClass("features__item_prev");
+        $featureItems.eq(checkNumber(featureCurrent + 1)).removeClass("features__item_next").addClass("features__item_active");
+        $featureItems.eq(checkNumber(featureCurrent - 1)).removeClass("features__item_prev");
+        $featureItems.eq(checkNumber(checkNumber(featureCurrent + 1) + 1)).addClass("features__item_next");
+        console.log(checkNumber(checkNumber(featureCurrent + 1)) + 1);
+    });
+    $(".navbar-toggle").click(function () {
+        $("#header__nav").toggle(500);
+    });
+});
